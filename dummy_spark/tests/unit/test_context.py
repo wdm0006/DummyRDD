6e54c7c32f028b75ceb7602581c6402778cbfb27
Henrique Souza <htssouza@gmail.com>
2016-06-13 11:44:04 -0300

    Finish implementing unit tests on SparkContext

--- dummy_spark/tests/unit/test_context.py
+++ dummy_spark/tests/unit/test_context.py
@@ -3,12 +3,21 @@
 import unittest
 
 from datetime import datetime
+from tempfile import NamedTemporaryFile
 
 from dummy_spark import SparkContext
 from dummy_spark import RDD
 
 
 class SparkContextTests (unittest.TestCase):
+
+    TEST_RANGES = [
+        (0, 10, 1),
+        (0, 10, 2),
+        (0, 100, 13),
+        (0, 1000, 17),
+        (0, 10000, 31),
+    ]
 
     def test_start_time(self):
         now = datetime.now()
@@ -27,45 +36,33 @@ class SparkContextTests (unittest.TestCase):
 
     def test_range(self):
         ctx = SparkContext()
-
-        l1 = range(0, 10, 1)
-        rdd1 = ctx.range(0, 10, 1)
-        self.assertEquals(l1, rdd1.collect())
-
-        l2 = range(0, 10, 2)
-        rdd2 = ctx.range(0, 10, 2)
-        self.assertEquals(l2, rdd2.collect())
-
-        l3 = range(0, 1000, 17)
-        rdd3 = ctx.range(0, 1000, 17)
-        self.assertEquals(l3, rdd3.collect())
+        for start, stop, step in self.TEST_RANGES:
+            l = range(start, stop, step)
+            rdd = ctx.range(start, stop, step)
+            self.assertEquals(l, rdd.collect())
 
     def test_parallelize_list(self):
         ctx = SparkContext()
-
-        l1 = range(0, 10, 1)
-        rdd1 = ctx.parallelize(l1)
-        self.assertEquals(l1, rdd1.collect())
-
-        l2 = range(0, 10, 2)
-        rdd2 = ctx.parallelize(l2)
-        self.assertEquals(l2, rdd2.collect())
-
-        l3 = range(0, 1000, 17)
-        rdd3 = ctx.parallelize(l3)
-        self.assertEquals(l3, rdd3.collect())
+        for start, stop, step in self.TEST_RANGES:
+            l = range(start, stop, step)
+            rdd = ctx.parallelize(l)
+            self.assertEquals(l, rdd.collect())
 
     def test_parallelize_set(self):
         ctx = SparkContext()
+        for start, stop, step in self.TEST_RANGES:
+            l = range(start, stop, step)
+            rdd = ctx.parallelize(set(l))
+            self.assertEquals(sorted(l), sorted(rdd.collect()))
 
-        l1 = range(0, 10, 1)
-        rdd1 = ctx.parallelize(set(l1))
-        self.assertEquals(sorted(l1), sorted(rdd1.collect()))
-
-        l2 = range(0, 10, 2)
-        rdd2 = ctx.parallelize(set(l2))
-        self.assertEquals(sorted(l2), sorted(rdd2.collect()))
-
-        l3 = range(0, 1000, 17)
-        rdd3 = ctx.parallelize(set(l3))
-        self.assertEquals(sorted(l3), sorted(rdd3.collect()))
+    def test_text_file(self):
+        ctx = SparkContext()
+        for start, stop, step in self.TEST_RANGES:
+            with NamedTemporaryFile(mode='w') as f:
+                l = ['{}\n'.format(x) for x in range(start, stop, step)]
+                for x in l:
+                    f.write(x)
+                f.flush()
+                f.seek(0)
+                rdd = ctx.textFile(f.name)
+                self.assertEquals(l, rdd.collect())
